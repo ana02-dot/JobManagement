17736ea959d7a9b5e42c5d165b49d08fc767ab72
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-08-13 17:04:36 +0400

    change code to top-level statement

--- JobManagement.API/Program.cs
+++ JobManagement.API/Program.cs
@@ -1,32 +1,57 @@
-namespace JobManagement.API;
+using JobManagement.Application.Interfaces;
+using JobManagement.Application.Services;
+using JobManagement.Infrastructure.Data;
+using JobManagement.Infrastructure.Repositories;
+using JobManagement.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Database
+builder.Services.AddDbContext<JobManagementDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IJobRepository, JobRepository>();
+builder.Services.AddScoped<IJobApplicationRepository, JobApplicationRepository>();
+
+// Services
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<JobService>();
+builder.Services.AddScoped<JobApplicationService>();
+
+// External Services
+builder.Services.AddHttpClient<IPersonalNumberVerificationService, GeorgianPersonalNumberVerificationService>();
+
+// CORS
+builder.Services.AddCors(options =>
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+    options.AddPolicy("AllowAll",
+        policy =>
         {
-            app.MapOpenApi();
-        }
+            policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
 
-        app.UseHttpsRedirection();
+var app = builder.Build();
 
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
