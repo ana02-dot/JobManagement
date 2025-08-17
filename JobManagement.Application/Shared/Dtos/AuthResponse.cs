6b635442b5cbd69e7e08b706d0de0e57838da0a6
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-08-17 19:31:13 +0400

    create authorization response dto

---
+++ JobManagement.Application/Shared/Dtos/AuthResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace JobManagement.Application.Dtos;
+
+public class AuthResponse
+{
+    public string Token { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+    public UserInfo User { get; set; } = null!;
+}
