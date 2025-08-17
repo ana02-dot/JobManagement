fd52a27c49308fc47709551a49efde085d10ff22
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-08-17 19:25:52 +0400

    create external dtos for clean structure

---
+++ JobManagement.Application/Shared/Dtos/LoginResponse.cs
@@ -0,0 +1,17 @@
+﻿using JobManagement.Domain.Enums;
+
+namespace JobManagement.Application.Dtos;
+
+public class LoginResponse
+{
+    public int UserId { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public UserRole Role { get; set; }
+    public string Token { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+    public bool IsEmailVerified { get; set; }
+    public bool IsPhoneVerified { get; set; }
+}
---
+++ JobManagement.Application/Shared/Dtos/UserRegistrationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace JobManagement.Application.Dtos;
+
+public class UserRegistrationResponse
+{
+    public int UserId { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public bool IsPhoneNumberVerified { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
