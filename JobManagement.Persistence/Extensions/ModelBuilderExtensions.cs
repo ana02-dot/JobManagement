966afb02d76770bdc3fd48fe5edeb43923e9f24b
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-08-17 19:34:28 +0400

    add baseentity configuration

---
+++ JobManagement.Persistence/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using JobManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobManagement.Persistence.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void ConfigureBaseEntity<T>(this ModelBuilder modelBuilder) where T : BaseEntity
+    {
+        modelBuilder.Entity<T>(entity =>
+        {
+            // Configure BaseEntity properties
+            entity.Property(e => e.Id).IsRequired();
+            entity.Property(e => e.CreatedAt).IsRequired();
+            entity.Property(e => e.CreatedBy).IsRequired();
+            entity.Property(e => e.UpdatedAt).IsRequired(false);
+            entity.Property(e => e.UpdatedBy).HasMaxLength(200);
+            entity.Property(e => e.IsDeleted).HasDefaultValue(false);
+
+            // Configure BaseEntity Creator relationship
+            entity.HasOne(e => e.Creator)
+                .WithMany()
+                .HasForeignKey(e => e.CreatedBy)
+                .OnDelete(DeleteBehavior.Restrict)
+                .IsRequired();
+        });
+    }
+}
