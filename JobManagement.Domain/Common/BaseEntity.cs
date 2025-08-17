b34bac43a38202315280af267557aca7f58443d2
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-08-17 17:37:18 +0400

    change and add props
    change CreatedBy prop type as int and add navigation prop

--- JobManagement.Domain/Common/BaseEntity.cs
+++ JobManagement.Domain/Common/BaseEntity.cs
@@ -1,11 +1,14 @@
-﻿namespace JobManagement.Domain.Common;
+﻿using JobManagement.Domain.Entities;
+
+namespace JobManagement.Domain.Common;
 
 public abstract class BaseEntity
 {
     public int Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
-    public string CreatedBy { get; set; } = null!;
+    public int CreatedBy { get; set; }
     public string? UpdatedBy { get; set; }
     public bool IsDeleted { get; set; }
+    public virtual User? Creator { get; set; }  // Navigation property
 }
