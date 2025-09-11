6bcb72dc4954ade055ce4530738198baecc12b61
ana02-dot <ana.chkhutiashvili001@gmail.com>
2025-09-11 18:21:26 +0400

    fix prop types

--- JobManagement.Domain/Common/BaseEntity.cs
+++ JobManagement.Domain/Common/BaseEntity.cs
@@ -5,10 +5,10 @@ namespace JobManagement.Domain.Common;
 public abstract class BaseEntity
 {
     public int Id { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
-    public int CreatedBy { get; set; }
+    public int? CreatedBy { get; set; }
     public string? UpdatedBy { get; set; }
-    public bool IsDeleted { get; set; }
+    public int IsDeleted { get; set; } = 0;
     public virtual User? Creator { get; set; }
 }
